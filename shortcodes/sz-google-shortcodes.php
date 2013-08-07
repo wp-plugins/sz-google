b619dca9cc5bede60731be653d4609a893b680e6
massimodellarovere <massimodellarovere@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-08-07 04:18:40 +0000

    Update SZ-Google 0.7
    git-svn-id: https://plugins.svn.wordpress.org/sz-google/tags/0.7@752705 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ shortcodes/sz-google-shortcodes.php
@@ -0,0 +1,20 @@
+<?php
+/* ************************************************************************** */
+/* Controllo se definita la costante del plugin                               */
+/* ************************************************************************** */
+if (!defined('SZ_PLUGIN_GOOGLE') or !SZ_PLUGIN_GOOGLE) die();
+
+/* ************************************************************************** */
+/* Definizione costanti che devo essere comuni a tutti i file admin           */
+/* ************************************************************************** */
+
+define('SZ_PLUGIN_GOOGLE_SHORTCODES',true);
+define('SZ_PLUGIN_GOOGLE_SHORTCODES_BASENAME',basename(__FILE__));
+
+/* ************************************************************************** */ 
+/* Controllo le opzioni generali per saper i moduli che devo essere caricati  */
+/* ************************************************************************** */ 
+
+$options_shortcodes = sz_google_modules_options();
+
+if ($options_shortcodes['plus']      == '1') @require_once(dirname(__FILE__).'/sz-google-shortcodes-plus.php');
